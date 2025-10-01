fb070adf23f3afc1d7ed41a493c315d416ef086e
Laura Vogel <laura.vogel@davinci.edu.ar>
2025-10-01 18:54:47 -0300

    feat(cargarPaciente): se agrego spinner de nivel educativo

--- app/src/main/java/com/example/psicopedagogiaandroid/cargarPaciente.java
+++ app/src/main/java/com/example/psicopedagogiaandroid/cargarPaciente.java
@@ -13,13 +13,17 @@ import androidx.core.view.WindowInsetsCompat;
 import java.lang.reflect.Array;
 
 public class cargarPaciente extends AppCompatActivity {
-
+    private Spinner nivelEdu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cargar_paciente);
 
+        nivelEdu = findViewById(R.id.nivelEdu);
+        String []opciones = {"Inicial", "Primario", "Secundario", "Terciario", "Universitario", "Posgrado"};
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
+        nivelEdu.setAdapter(adaptador);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
