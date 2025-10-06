a2d82130c17969fe2189861fd998b24877a58339
Laura Vogel <laura.vogel@davinci.edu.ar>
2025-10-06 19:47:24 -0300

    feat(cargarPaciente): se agrego fecha de nacimiento y se agregaron elementos al spinner de nivel educativo

--- app/src/main/java/com/example/psicopedagogiaandroid/cargarPaciente.java
+++ app/src/main/java/com/example/psicopedagogiaandroid/cargarPaciente.java
@@ -1,7 +1,9 @@
 package com.example.psicopedagogiaandroid;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.lang.reflect.Array;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class cargarPaciente extends AppCompatActivity {
     private Spinner nivelEdu;
@@ -24,6 +28,30 @@ public class cargarPaciente extends AppCompatActivity {
         String []opciones = {"Inicial", "Primario", "Secundario", "Terciario", "Universitario", "Posgrado"};
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
         nivelEdu.setAdapter(adaptador);
+
+        EditText fechaNac = findViewById(R.id.fechaNac);
+        Spinner nivelEdu = findViewById(R.id.nivelEdu);
+
+// DatePicker
+        fechaNac.setOnClickListener(v -> {
+            final Calendar c = Calendar.getInstance();
+            int y = c.get(Calendar.YEAR);
+            int m = c.get(Calendar.MONTH);
+            int d = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog dp = new DatePickerDialog(
+                    this,
+                    (view, year, month, dayOfMonth) -> {
+                        // formateo simple DD/MM/YYYY
+                        String txt = String.format(Locale.getDefault(), "%02d/%02d/%04d",
+                                dayOfMonth, month + 1, year);
+                        fechaNac.setText(txt);
+                    },
+                    y, m, d
+            );
+            dp.show();
+        });
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
